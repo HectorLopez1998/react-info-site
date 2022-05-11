ba52734cf17b76216871eadb5cb685ca8c9ce227
HectorLopez1998 <hectorlopezp1998@gmail.com>
2022-05-11 11:55:51 -0400

    added components folder

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ src/components/MainContent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function MainContent() {
+  return (
+    <main>
+      <h1>Reasons I'm learning React</h1>
+      <section>
+        <ul>
+          <li>Reason number one</li>
+          <li>Reason number two</li>
+          <li>Reason number three</li>
+        </ul>
+      </section>
+    </main>
+  )
+}
