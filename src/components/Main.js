0dffc9ed3ad49b887dccaf694ed868f741fe95b8
HectorLopez1998 <hectorlopezp1998@gmail.com>
2022-05-12 08:20:56 -0400

    commit tree d9d5b04ad8da2e16e22d9770f65b466cc29122a4

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,14 +3,15 @@ import React from 'react'
 export default function Main() {
   return (
     <main>
-      <h1>Fun Facts about React</h1>
-      <section>
-        <ul>
-          <li>Reason number one</li>
-          <li>Reason number two</li>
-          <li>Reason number three</li>
-        </ul>
-      </section>
+      <h1 className="main--title">Fun Facts about React</h1>
+
+      <ul className='main--list'>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </main>
   )
 }
